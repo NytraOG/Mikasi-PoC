040de02b74d9039b4dffce5abfb8aa94349bb673
Christian Nytra <ch.nytra.13@gmail.com>
2026-05-23 12:35:01 +0200

    rename ugh

---
+++ Blackshield/Client/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,7 @@
+namespace Blackshield.Components.Account;
+
+public class PasskeyInputModel
+{
+    public string? CredentialJson { get; set; }
+    public string? Error          { get; set; }
+}
---
+++ Blackshield/Client/Data/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blackshield.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) { }
