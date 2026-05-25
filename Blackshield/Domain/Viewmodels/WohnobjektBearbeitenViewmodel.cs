9bcd4bf1195a2d58e58b17e71ebbf87fa95b99cb
Christian Nytra <ch.nytra.13@gmail.com>
2026-05-25 18:29:54 +0200

    unterbrechung

---
+++ Blackshield/Domain/Viewmodels/WohnobjektBearbeitenViewmodel.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Data.Enums;
+
+namespace Domain.Viewmodels;
+
+public class WohnobjektBearbeitenViewmodel
+{
+    public int         AnzahlZimmer                  { get; set; }
+    public string?     BezeichnungWirtschaftseinheit { get; set; }
+    public string?     BezeichnungNutzungseinheit    { get; set; }
+    public string?     Etage                         { get; set; }
+    public DateOnly    FrühesterEinzugAb             { get; set; }
+    public string?     Hausnummer                    { get; set; }
+    public decimal     Heizkosten                    { get; set; }
+    public decimal     Kaltmiete                     { get; set; }
+    public decimal     Kaution                       { get; set; }
+    public decimal     Nebenkosten                   { get; set; }
+    public string?     Ort                           { get; set; }
+    public string?     PLZ                           { get; set; }
+    public string?     Straße                        { get; set; }
+    public Wohnungstyp Typ                           { get; set; }
+    public Mietstatus  Status                        { get; set; }
+    public decimal     Wohnfläche                    { get; set; }
+    public string?     Beschreibung                  { get; set; }
+}
